27936f111268dcd532187864e5e7f8223096edfe
Brian Keane <brian@ctkeane.com>
2020-04-30 21:38:23 -0400

    after markdown video

--- markdown-editor/start/src/App.js
+++ markdown-editor/start/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+//import marked from 'marked';
+import ReactMarkdown from 'react-markdown'; 
 import './App.css';
 
 export default function App() {
+  const [markdown, setMarkdown] = useState('# Sup');
+
+  function handleChange(e){
+    setMarkdown(e.target.value);
+  }
+
   return (
     <div className="app">
-      <textarea />
+      <textarea
+        onChange={handleChange}
+        value={markdown} 
+      />
+{/*
+      < div className = "preview"
+      dangerouslySetInnerHTML = {{ __html: marked(markdown)}} >
+         </div>
+     
+  */}
 
-      <div className="preview" />
-    </div>
+  <ReactMarkdown className="preview" source={markdown} />
+ < /div>
+
   );
 }
